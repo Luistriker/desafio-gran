5feee05065bb9fa5075537ebf894c676b627059c
Easjur <escoladpdf@gmail.com>
2021-12-16 12:53:47 -0300

    feat: first code

---
+++ script.js
@@ -0,0 +1,28 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".fa-ellipsis-v");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+$(window).scroll(function(){
+  if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+  }else{
+      $('.navbar').removeClass("sticky");
+  }
+  
+});
+
+// slide-up script
+$('.scroll-up-btn').click(function(){
+  $('html').animate({scrollTop: 0});
+});
